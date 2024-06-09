cf1d7874095b5eda6601e04527090d60758b3749
jagadeeshc1 <chinnasettyjagadeesh@gmail.com>
2024-06-09 17:22:14 +0530

    case when approach

--- src/main/kotlin/ReflectionTypeUseCase.kt
+++ src/main/kotlin/ReflectionTypeUseCase.kt
@@ -1,6 +1,8 @@
 package org.example
 
+import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.Encoders
+import org.apache.spark.sql.Row
 import org.apache.spark.sql.functions.*
 
 
@@ -20,8 +22,7 @@ data class Section(
     var mandatoryScores:List<String>
 )
 
-fun main(){
-
+fun getStudentDs():Dataset<Student>{
     val sparkSession = getSparkSession()
 
     val student1 = Student(1,100,"student1",50,60,70)
@@ -31,11 +32,14 @@ fun main(){
     val students = listOf(
         student1,
         student2,
-        student2
+        student3
     )
 
-    val studentDs = sparkSession.createDataset(students,Encoders.bean(Student::class.java))
+    return sparkSession.createDataset(students,Encoders.bean(Student::class.java))
+}
 
+fun getSectionDs():Dataset<Section>{
+    val sparkSession = getSparkSession()
     val section1 = Section(100, listOf("mathScore","englishScore"))
     val section2 = Section(200, listOf("englishScore","scienceScore"))
 
@@ -44,25 +48,46 @@ fun main(){
         section2
     )
 
-    val sectionDs = sparkSession.createDataset(sections,Encoders.bean(Section::class.java))
+    return sparkSession.createDataset(sections,Encoders.bean(Section::class.java))
 
-    // for each student get the sum of the mandatory subjects
+}
 
+fun simpleScoreSum(studentDs:Dataset<Student>){
+    val resultDf = studentDs
+        .withColumn("totalScore",
+            col("englishScore").plus(col("scienceScore")).plus(col("mathScore")))
+
+    /**
+     * or
+     * studentDs.withColumn("totalScore",expr("englishScore + scienceScore + mathScore"))
+     *
+     * both are same
+     */
+
+    resultDf.show(false)
+
+}
+
+
+fun getJoinedDs():Dataset<Row>{
+    val studentDs = getStudentDs()
+    val sectionDs = getSectionDs()
     val joinedDs = studentDs.join(sectionDs,"sectionId")
+    joinedDs.show(false)
+    return joinedDs
 
-//    joinedDs.show(false)
+}
 
+fun mandatoryScoreSumUsingGeneratedCaseWhen(joinedDs:Dataset<Row>){
     val cols = joinedDs.columns().toSet()
-
-    var caseExpr = "CASE "
+    var caseExpr = "CASE \n"
     for(colName in cols){
-        caseExpr += " WHEN x= '$colName' then cast(cast($colName as string) as int) "
+        caseExpr += " WHEN x= '$colName' then cast(cast($colName as string) as int) \n"
     }
-    caseExpr += " ELSE null"
-    caseExpr += " END"
+    caseExpr += " ELSE null \n"
+    caseExpr += "END \n"
 
     println(caseExpr)
-
 
     val result = joinedDs.withColumn(
         "mandatoryScoreValues",
@@ -80,12 +105,18 @@ fun main(){
     )
 
     result.show(false)
+}
 
 
+fun main(){
 
+    val studentDs = getStudentDs()
+    val sectionDs = getSectionDs()
 
+//    simpleScoreSum(studentDs)
 
+    val joinedDs = getJoinedDs()
 
-
+    mandatoryScoreSumUsingGeneratedCaseWhen(joinedDs)
 
 }
